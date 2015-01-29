f16236032e2f80319d41dca49921aec9f9ecbae5
Simon Ho <simon@strongloop.com>
2015-01-29 02:53:17 -0800

    Add map to Locations tab

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -8,6 +8,31 @@ angular.module('app.controllers', [])
     Locations.remove(location);
   }
 })
+.controller('MapCtrl', function($scope, $ionicLoading) {
+  $scope.mapCreated = function(map) {
+    $scope.map = map;
+  };
+
+  $scope.centerOnMe = function () {
+    console.log("Centering");
+    if (!$scope.map) {
+      return;
+    }
+
+    $scope.loading = $ionicLoading.show({
+      content: 'Getting location...',
+      showBackdrop: false
+    });
+
+    navigator.geolocation.getCurrentPosition(function (pos) {
+      console.log('Got pos', pos);
+      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      $scope.loading.hide();
+    }, function (error) {
+      alert('Unable to get location: ' + error.message);
+    });
+  };
+})
 
 .controller('FriendsCtrl', function($scope, Friends) {
   $scope.friends = Friends.all();
@@ -22,3 +47,5 @@ angular.module('app.controllers', [])
     enableFriends: true
   };
 });
+
+
